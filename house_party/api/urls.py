7cc323bd3f07acd5a977673c145c2a20ce67f83c
Lucas Voltera <lucasvoltera@gmail.com>
2024-01-09 16:37:25 -0300

    feat: add room

--- house_party/api/urls.py
+++ house_party/api/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import main
+from .views import RoomView
 
 urlpatterns = [
-    path('', main)
+    path('home/', RoomView.as_view()),
 ]
