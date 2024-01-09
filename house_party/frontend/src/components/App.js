1eaae4ade2115497b2c1912b57c64ce79fd705c0
Lucas Voltera <lucasvoltera@gmail.com>
2024-01-09 19:31:36 -0300

    feat: homepage, createroom and roomjoin components

--- house_party/frontend/src/components/App.js
+++ house_party/frontend/src/components/App.js
@@ -1,16 +1,23 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import HomePage from "./Homepage";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
 
 export default class App extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <h1>Test React code</h1>
-        )
-    }
+  render() {
+    return (
+      <div className="center">
+        <HomePage />
+        {/* <RoomJoinPage />
+        <CreateRoomPage /> */}
+      </div>
+    );
+  }
 }
 
 const appDiv = document.getElementById("app");
